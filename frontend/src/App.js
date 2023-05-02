a4ccc5fcd6f79644a94916b757f7d266181e5a16
Oliver Gooding <olliejgooding@gmail.com>
2023-05-02 16:38:18 +0100

    both ends

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+
+    fetch(`/api`)
+      .then(res => res.json())
+      .then(data => {
+        setNews(data);
+      })
+      .catch(error => console.log(error));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Thing should appear here:
+
+          {news.brand}
+
+
         </p>
         <a
           className="App-link"
